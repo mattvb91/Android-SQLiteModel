63d4cd49a485a1557905e8881b604fee7b683611
Matthias von Bargen <matt@mavon.ie>
2017-11-07 18:02:06 +0100

    Basic test example

--- sqlitemodel/src/test/java/ie/mavon/sqlitemodel/SQLiteModelUnitTest.java
+++ sqlitemodel/src/test/java/ie/mavon/sqlitemodel/SQLiteModelUnitTest.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
+import ie.mavon.sqlitemodel.Models.Employee;
+
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +19,24 @@ public class SQLiteModelUnitTest {
     @Before
     public void setUp() {
         SQLiteModel.setDB(new TestDBHelper(RuntimeEnvironment.application, "testDb", null, 1));
+
+        SQLiteModel.getDbHandler().getWritableDatabase().execSQL("DROP TABLE IF EXISTS employees");
+        SQLiteModel.getDbHandler().getWritableDatabase().execSQL("CREATE TABLE employees (_id INTEGER PRIMARY KEY, name TEXT)");
     }
 
     @Test
     public void getDatabaseName() throws Exception {
         assertEquals("testDb", SQLiteModel.getDbHandler().getDatabaseName());
+    }
+
+    @Test
+    public void testExists()
+    {
+        Employee employee = new Employee();
+        employee.setName("Joe Blogs");
+        assertTrue(employee.save());
+
+        assertTrue(employee.exists());
+
     }
 }
